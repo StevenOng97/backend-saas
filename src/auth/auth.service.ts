ada360700130e49d8d73990721a8ff13311ebb70
StevenOng97 <giaanguyen221197@gmail.com>
2025-05-20 12:11:15 +0700

    Update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,8 +28,9 @@ export class AuthService {
     response.cookie('access_token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000, // 1 day
+      domain: '.onrender.com',
       path: '/',
     });
   }
